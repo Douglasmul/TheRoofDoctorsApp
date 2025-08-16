2ef7da85aaab3f72d5ce1c49b9c552a1dcfa19ca
Douglasmul <douglasjmulcahey@gmail.com>
2025-08-15 23:34:59 -0400

    Add stack navigator for OpenApp and MeasureRoofScreen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import HomeScreen from '../screens/HomeScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import OpenAppScreen from '../screens/OpenApp';
 import MeasureRoofScreen from '../screens/MeasureRoofScreen';
-import QuoteScreen from '../screens/QuoteScreen';
 
-export type RootStackParamList = {
-  Home: undefined;
-  MeasureRoof: undefined;
-  Quote: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'The Roof Doctors' }} />
+      <Stack.Navigator initialRouteName="OpenApp">
+        <Stack.Screen name="OpenApp" component={OpenAppScreen} options={{ headerShown: false }} />
         <Stack.Screen name="MeasureRoof" component={MeasureRoofScreen} options={{ title: 'Measure Roof' }} />
-        <Stack.Screen name="Quote" component={QuoteScreen} options={{ title: 'Quote' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
